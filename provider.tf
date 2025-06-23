d9b0cf359d1209a5989c70ef8ae8f60d30dd7e94
ankitamohanty1509 <148987768+ankitamohanty1509@users.noreply.github.com>
2025-06-23 15:44:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
